1232da8d9409e1581c3c498ac4fa50e9b529f82d
Harshs0907 <150027883+Harshs0907@users.noreply.github.com>
2024-11-15 14:05:50 +0000

    conversionoperation

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,24 @@
+let score = "33";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+ let somenumber = 33;
+  let stringnumber = String(somenumber)
+  console.log(stringnumber);
+
+// *****************************Operations*************************************************
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+let str1 = "harsh";
+let str2 = " rana";
+let str3 = (str1 + str2);
+console.log(str3);
+
+console.log(1 + "2");
+console.log(1 + 2 + "2");
+console.log("1" + "2" + 2);
