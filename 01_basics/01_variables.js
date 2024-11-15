4572efe9b39acb0d771af4160457016175b34ffb
Harshs0907 <150027883+Harshs0907@users.noreply.github.com>
2024-11-15 12:48:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144533
+let accountEmail = "harsh09@gmail.com"
+var accountPassword = "12345"
+accountCity = "Noida"
+
+// accountId = 2
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; /*treat all js code as new version */
+
+//alert(3+3)  // we are using nodejs not browser
+
+console.log(3 + 3);
+let name = "harsh"
+let age = 20
+let isloggedin = false
+
+
+//number => 2^53
+// bigint
+// boolean => true/false
+// null => standalone value 
+//undefined 
+// symbol => for uniqueness finding 
+
+
+// objects
+console.log(typeof null)
