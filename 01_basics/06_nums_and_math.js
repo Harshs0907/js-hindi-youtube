a77d581adeddae638108ddc381e930531549a4a9
Harshs0907 <150027883+Harshs0907@users.noreply.github.com>
2024-11-21 17:58:41 +0000

    done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,18 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.8534
+
+console.log(otherNumber.toPrecision(4));
+
+// ++++++++++++++++++++MAths+++++++++++++++++++++++++++++=
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6))
+console.log(Math.max(3,5,1,8,4));
+
