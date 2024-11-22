1dcb1ce4743f8e2f598074753746cf3b592d802b
Harshs0907 <150027883+Harshs0907@users.noreply.github.com>
2024-11-22 17:19:33 +0000

    nums done

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,18 +1,28 @@
 const score = 400
-console.log(score);
+//console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+//console.log(balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
 
 const otherNumber = 123.8534
 
-console.log(otherNumber.toPrecision(4));
+//console.log(otherNumber.toPrecision(4));
+//const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
 
 // ++++++++++++++++++++MAths+++++++++++++++++++++++++++++=
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4.6))
-console.log(Math.max(3,5,1,8,4));
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.6))
+//console.log(Math.max(3,5,1,8.6,4));
 
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min)
